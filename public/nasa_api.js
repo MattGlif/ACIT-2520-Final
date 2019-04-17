67d3508eba0879366577476c55f46c8cb6a22efb
MatthewGlifonea <matthewglifonea@gmail.com>
2019-04-17 12:39:41 -0700

    nasa finishing touches

--- public/nasa_api.js
+++ public/nasa_api.js
@@ -2,23 +2,61 @@ const axios = require('axios');
 const _ = require('lodash');
 const nasa_key = 'IrLWAsam5NjXMSuczqAFuQTPmhjpviYBoOUMIBlj'
 
+// var get_pictures = (query) => {
+//     return new Promise(async (resolve, reject) => {
+//         try {
+//             const pics = await axios.get(`https://images-api.nasa.gov/search?q=${query}`);
+//             let nasa_pic = [];
+
+//             for (pic in pictures.data.collection.items){
+//                 picture = pictures.data.collections.items[0].links[0].href;
+//                 if (picture.substring(0, 36) === 'https://images-assets.nasa.gov/image')
+//                 nasa_pic.push(picture);
+//             }
+//             resolve(nasa_pic)
+//         } catch (e) {
+//             reject (e)
+//         }
+//     })
+// };
+// var get_pictures = (query) => {
+//     return new Promise(async (resolve, reject) => {
+//         try {
+//             const pics = await axios.get(`https://images-api.nasa.gov/search?q=${query}`);
+//             let nasa_pic = [];
+//             for (pic in pictures.data.collection.items){
+//                 e = pics.data.collection.items[pic].links[0].href;
+//                 // console.log(e.substring(0,36));
+//                 if (e.substring(0,36) === 'https://images-assets.nasa.gov/image')
+//                     nasa_pic.push(e);
+//             }
+//             resolve(nasa_pic)
+//         } catch (e) {
+
+//             reject(e);
+//         }
+//     })
+// };
+
 var get_pictures = (query) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const pics = await axios.get(`https://images-api.nasa.gov/search?q=${query}`);
-            let nasa_pic = [];
-            let picture = pictures.data.collections.items[0].links[0].href;
-
-            for (pic in pictures.data.collection.items){
-                if (picture.substring(0, 36) === 'https://images-assets.nasa.gov/image')
-                nasa_pic.push(picture);
+            let output = [];
+            const pictures = await axios.get(`https://images-api.nasa.gov/search?q=${query}`);
+            for (i in pictures.data.collection.items){
+                pic = pictures.data.collection.items[i].links[0].href;
+                // console.log(e.substring(0,36));
+                if (pic.substring(0,36) === 'https://images-assets.nasa.gov/image')
+                    output.push(pic);
             }
-            resolve(nasa_pic)
-        } catch (err) {
-            reject (err)
+            resolve(output)
+        } catch (e) {
+
+            reject(e);
         }
     })
 };
+
 
 module.exports = {
     get_pictures
--- server.js
+++ server.js
@@ -35,7 +35,23 @@ app.get('/nasa', (request, response) => {
 })
 
 app.post('/nasa', urlencode, async(request, response) => {
-    response.render('nasa_form.hbs')
+    try {
+        if (request.body.picture_input === null) throw "Please Enter A Valid Item";
+        let NASAoutput = await nasa_api.get_pictures(request.body.picture_input);
+
+        response.render('nasa_form.hbs', {
+            output1: NASAoutput[0],
+            output2: NASAoutput[1],
+            output3: NASAoutput[2],
+            output4: NASAoutput[3],
+            output5: NASAoutput[4],
+            output6: NASAoutput[5]
+        });
+    }catch (e) {
+        response.render('nasa_form.hbs', {
+            output: e
+        });
+    }
 })
 
 // app.listen(process.env.PORT, '0.0.0.0')
